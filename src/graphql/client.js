fcb71b659f619571147b9ba754a5a324bb05ea98
Masao Kitamura <masaok@gmail.com>
2023-07-06 01:42:35 -0700

    Using new endpoint works

--- src/graphql/client.js
+++ src/graphql/client.js
@@ -1,7 +1,11 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, InMemoryCache } from '@apollo/client'
 const { REACT_APP_API_ROOT } = process.env
 
 export const client = new ApolloClient({
-  uri: `${REACT_APP_API_ROOT}/graphql`,
-  cache: new InMemoryCache()
-});
+  // uri: `${REACT_APP_API_ROOT}/graphql`,
+
+  // The GraphQL endpoint moved temporarily to a new URL:
+  // https://github.com/SpaceXLand/api/issues/241#issuecomment-1403255116
+  uri: 'https://main--spacex-l4uc6p.apollographos.net/graphql',
+  cache: new InMemoryCache(),
+})
